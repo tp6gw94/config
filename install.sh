74991ec3644253ae157ea65c575f7fb664487e52
todd <tp6gw94@gmail.com>
2024-07-20 10:48:45 +0800

    add install

--- install.sh
+++ install.sh
@@ -3,5 +3,10 @@
 brew install neovim asdf gnu-sed tmux lazygit zoxide fzf ripgrep starship
 brew install --cask font-jetbrains-mono-nerd-font raycast
 
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 -y
+
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs latest
+asdf global nodejs latest
