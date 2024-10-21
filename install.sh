f4d1ec9a6c6cda8aecc4d903c22474eedc560505
Todd Chen <toddchen@noodoe.com>
2024-10-21 10:52:42 +0800

    lazygit command

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
-!/bin/bash
+#!/bin/bash
 
 brew install neovim asdf gnu-sed tmux lazygit zoxide fzf ripgrep starship
 brew install --cask font-jetbrains-mono-nerd-font raycast
 brew install yazi ffmpegthumbnailer sevenzip jq poppler fd ripgrep fzf zoxide imagemagick font-symbols-only-nerd-font
 brew install helix
 brew install gh
-
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 -y
 
