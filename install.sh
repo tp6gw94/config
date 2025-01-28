0679117b6c2328b3c04ba7101d22a680e145fce4
toddchen <tp6gw94@gmail.com>
2025-01-28 23:49:21 +0800

    zk

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ brew install --cask font-jetbrains-mono-nerd-font raycast
 brew install yazi ffmpegthumbnailer sevenzip jq poppler fd ripgrep fzf zoxide imagemagick font-symbols-only-nerd-font
 brew install helix
 brew install gh
+brew install zk slides
 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 -y
 
@@ -17,4 +18,3 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install --locked zellij
 
 npm install -g graphql-language-service pnpm yarn typescript-language-server typescript vscode-langservers-extracted prettier
-
