c3f313dd94fce0d51bd99660bf285b57ff56c80c
toddchen <tp6gw94@gmail.com>
2025-02-15 18:47:29 +0800

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,129 +1,147 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = 'One Light (base16)'
-config.font = wezterm.font 'JetBrains Mono'
+local light_scheme = "One Light (base16)"
+local dark_scheme = "One Dark (base16)"
+wezterm.on("toggle-color-scheme", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	if overrides.color_scheme == light_scheme then
+		overrides.color_scheme = dark_scheme
+	else
+		overrides.color_scheme = light_scheme
+	end
+	window:set_config_overrides(overrides)
+end)
+
+config.color_scheme = "One Light (base16)"
+config.font = wezterm.font("JetBrains Mono")
 
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1500 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1500 }
 config.keys = {
-  -- pane split
-  {
-    key = '|',
-    mods = 'LEADER|SHIFT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = '_',
-    mods = 'LEADER|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'z',
-    mods = 'LEADER',
-    action = wezterm.action.TogglePaneZoomState,
-  },
-  -- panee move
-  {
-    key = 'h',
-    mods = 'LEADER',
-    action = wezterm.action.ActivatePaneDirection 'Left'
-  },
-  {
-    key = 'j',
-    mods = 'LEADER',
-    action = wezterm.action.ActivatePaneDirection 'Down'
-  },
-  {
-    key = 'k',
-    mods = 'LEADER',
-    action = wezterm.action.ActivatePaneDirection 'Up'
-  },
-  {
-    key = 'l',
-    mods = 'LEADER',
-    action = wezterm.action.ActivatePaneDirection 'Right'
-  },
-  -- pane resize
-  {
-    key = 'H',
-    mods = 'LEADER',
-    action = wezterm.action.AdjustPaneSize { 'Left', 5 }
-  },
-  {
-    key = 'J',
-    mods = 'LEADER',
-    action = wezterm.action.AdjustPaneSize { 'Down', 5 }
-  },
-  {
-    key = 'K',
-    mods = 'LEADER',
-    action = wezterm.action.AdjustPaneSize { 'Up', 5 }
-  },
-  {
-    key = 'L',
-    mods = 'LEADER',
-    action = wezterm.action.AdjustPaneSize { 'Right', 5 }
-  },
-  -- close pane
-  {
-    key = 'x',
-    mods = 'LEADER',
-    action = wezterm.action.CloseCurrentPane { confirm = true }
-  },
-  -- reload configuration
-  {
-    key = 'R',
-    mods = 'LEADER',
-    action = wezterm.action.ReloadConfiguration
-  },
-  -- tab
-  {
-    key = 'r',
-    mods = 'LEADER',
-    action = wezterm.action.PromptInputLine {
-      description = 'Enter new name for tab',
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end),
-    }
-  },
-  {
-    key = 't',
-    mods = 'LEADER',
-    action = wezterm.action.SpawnCommandInNewTab {
-      cwd = wezterm.home_dir
-    }
-  },
-  -- close tab
-  {
-    key = 'q',
-    mods = 'LEADER',
-    action = wezterm.action.CloseCurrentTab { confirm = true }
-  },
-  {
-    key = "LeftArrow",
-    mods = "OPT",
-    action = wezterm.action { SendString = "\x1bb" }
-  },
-  {
-    key = "RightArrow",
-    mods = "OPT",
-    action = wezterm.action { SendString = "\x1bf" }
-  }
+	-- pane split
+	{
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "_",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "z",
+		mods = "LEADER",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	-- panee move
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	-- pane resize
+	{
+		key = "H",
+		mods = "LEADER",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "J",
+		mods = "LEADER",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		key = "K",
+		mods = "LEADER",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "L",
+		mods = "LEADER",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	-- close pane
+	{
+		key = "x",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	-- reload configuration
+	{
+		key = "R",
+		mods = "LEADER",
+		action = wezterm.action.ReloadConfiguration,
+	},
+	-- tab
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "t",
+		mods = "LEADER",
+		action = wezterm.action.SpawnCommandInNewTab({
+			cwd = wezterm.home_dir,
+		}),
+	},
+	-- close tab
+	{
+		key = "q",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentTab({ confirm = true }),
+	},
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = wezterm.action({ SendString = "\x1bb" }),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = wezterm.action({ SendString = "\x1bf" }),
+	},
+	-- togle theme
+	{
+		key = "m",
+		mods = "CMD",
+		action = wezterm.action({ EmitEvent = "toggle-color-scheme" }),
+	},
 }
 
 for i = 1, 9 do
-  -- CTRL+ALT + number to activate that tab
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'LEADER',
-    action = wezterm.action.ActivateTab(i - 1),
-  })
+	-- CTRL+ALT + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
 end
 
 return config
