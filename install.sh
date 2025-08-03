1c08650de01490b6491ca0baba427060c7412503
Todd <chenshengtai@Mac.hitronhub.home>
2025-08-03 13:34:01 +0800

    script

--- install.sh
+++ install.sh
@@ -4,9 +4,13 @@ brew bundle install --file=~/.config/Brewfile
 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 -y
 
-curl https://get.volta.sh | bash
-volta install node
-
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 brew services start borders
+
+mise install
+
+cargo install taplo-cli --locked
+curl -fsSL https://tombi-toml.github.io/tombi/install.sh | sh
+
+npm install -g typescript-language-server typescript vscode-langservers-extracted @tailwindcss/language-server @vue/typescript-plugin @vue/language-server 
--- sync.sh
+++ sync.sh
@@ -1,14 +1,13 @@
-rsync -a helix config
 rsync -a .zshrc config
-rsync -a yazi config
 rsync -a helix config
+rsync -a yazi config
 rsync -a wezterm config
 rsync -a nvim config
-rsync -a slide-themes config
+rsync -a zk config
 rsync -a aerospace config
 rsync -a borders config
-rsync -a mise config
 cp starship.toml config
 cp ./sync.sh config
 cp ./install.sh config
 cp ./Brewfile config
+cp ./install.sh config
